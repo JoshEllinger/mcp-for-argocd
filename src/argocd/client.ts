7cf5d43dfe0ae282aeb2a05ad07eaf0fb460d7dc
Leonardo Luz Almeida <leoluz@users.noreply.github.com>
2026-07-09 16:41:46 -0400

    feat: add a new tool to fetch AppProject resources (#133)
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>

--- src/argocd/client.ts
+++ src/argocd/client.ts
@@ -8,7 +8,8 @@ import {
   V1alpha1ResourceDiff,
   V1alpha1ResourceResult,
   V1alpha1ApplicationResourceResult,
-  V1alpha1ClusterList
+  V1alpha1ClusterList,
+  V1alpha1AppProject
 } from '../types/argocd-types.js';
 import { HttpClient } from './http.js';
 import type { TokenRefreshProvider } from '../auth/token-refresh.js';
@@ -92,6 +93,11 @@ export class ArgoCDClient {
       `/api/v1/applications/${applicationName}`,
       queryParams
     );
+    return body;
+  }
+
+  public async getAppProject(projectName: string) {
+    const { body } = await this.client.get<V1alpha1AppProject>(`/api/v1/projects/${projectName}`);
     return body;
   }
 
